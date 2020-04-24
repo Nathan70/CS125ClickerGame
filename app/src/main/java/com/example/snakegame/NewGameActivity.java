3f72c7d314394be6525ee7d2fb97581274dfa94a
Nathan70 <nning3@illinois.edu>
2020-04-24 16:40:15 -0500

    Pause Button NewGameActivity

--- app/src/main/java/com/example/snakegame/NewGameActivity.java
+++ app/src/main/java/com/example/snakegame/NewGameActivity.java
@@ -3,12 +3,28 @@ package com.example.snakegame;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.snakegame.logic.Game;
 
 public class NewGameActivity extends AppCompatActivity {
+
+    /** instance of Game*/
+    Game game;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_game);
+
+        game = new Game(MainActivity.getBig());
+        Button pauseButton = findViewById(R.id.pauseButton);
+        pauseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                game.togglePause();
+            }
+        });
     }
 }
