41de28baa0f242f6648c50dff4d5cd74e7c1e3c9
tyang54 <tyang54@illinois.edu>
2020-05-02 16:05:32 -0500

    clicker ui

--- app/src/main/java/com/example/snakegame/ClickerGame.java
+++ app/src/main/java/com/example/snakegame/ClickerGame.java
@@ -3,12 +3,50 @@ package com.example.snakegame;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class ClickerGame extends AppCompatActivity {
+
+    /** earth clicker button */
+    ImageButton earthButton;
+
+    /** earth rotation animation */
+    Animation rotate;
+
+    /** moon clicker button */
+    ImageButton moonButton;
+
+    /** astronaut images */
+    ImageView a1;
+    ImageView a2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_clicker_game);
+
+        /** add rotation animation to earth button */
+        earthButton = findViewById(R.id.earthClicker);
+        rotate = AnimationUtils.loadAnimation(ClickerGame.this, R.anim.rotation);
+        earthButton.startAnimation(rotate);
+
+        /** add rotation animation to moon button when clicked */
+        moonButton = findViewById(R.id.moonClicker);
+        moonButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                moonButton.animate().rotation(moonButton.getRotation() + 360).start();
+            }
+        });
+
+        /** add rotation to astronauts */
+        a1 = findViewById(R.id.astronaut1);
+        a2 = findViewById(R.id.astronaut2);
+        a1.startAnimation(rotate);
+        a2.startAnimation(rotate);
     }
 }
