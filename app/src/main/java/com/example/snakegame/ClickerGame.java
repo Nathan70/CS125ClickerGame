7a9a314e009d7482b9808adfa04ac144adb35e6e
tyang54 <tyang54@illinois.edu>
2020-05-02 16:27:15 -0500

    clicker animation

--- app/src/main/java/com/example/snakegame/ClickerGame.java
+++ app/src/main/java/com/example/snakegame/ClickerGame.java
@@ -7,8 +7,11 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class ClickerGame extends AppCompatActivity {
@@ -33,6 +36,18 @@ public class ClickerGame extends AppCompatActivity {
      */
     private TextView multiplierText;
 
+    /** earth clicker button */
+    ImageButton earthButton;
+
+    /** earth rotation animation */
+    Animation rotate;
+
+    /** moon clicker button */
+    ImageButton moonButton;
+
+    /** astronaut images */
+    ImageView a1;
+    ImageView a2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,6 +82,26 @@ public class ClickerGame extends AppCompatActivity {
                 multiplier();
             }
         });
+
+        /** add rotation animation to earth button */
+        earthButton = findViewById(R.id.earthClicker);
+        rotate = AnimationUtils.loadAnimation(ClickerGame.this, R.anim.rotation);
+        earthButton.startAnimation(rotate);
+
+        /** add rotation animation to moon button when clicked */
+        moonButton = findViewById(R.id.moonClicker);
+        moonButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                moonButton.animate().rotation(moonButton.getRotation() + 360).start();
+            }
+        });
+
+        /** add rotation to astronauts */
+        a1 = findViewById(R.id.astronaut1);
+        a2 = findViewById(R.id.astronaut2);
+        a1.startAnimation(rotate);
+        a2.startAnimation(rotate);
     }
 
     /**
